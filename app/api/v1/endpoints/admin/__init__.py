75c1f69ddbe889a19de9c8d941c895dd1bfb1f06
moorerebecca301973-tech <moorerebecca301973@gmail.com>
2026-09-19 15:13:15 +0100

    Update __init__.py

--- app/api/v1/endpoints/admin/__init__.py
+++ app/api/v1/endpoints/admin/__init__.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
 
+from app.api.v1.endpoints.admin import attractions, bookings, categories
+
+router = APIRouter()
+router.include_router(attractions.router)
+router.include_router(categories.router)
+router.include_router(bookings.router)
